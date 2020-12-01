8dea21dc8ddab4178553f3ad53501df9bcbe9cf1
JPFreely <jpshoulihan@gmail.com>
2020-12-01 22:44:39 +0000

    components built based on users level mapped with dropdown button click toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Intermediate from "./components/Intermediate";
 
 class App extends Component {
   constructor(props) {
@@ -8,21 +9,32 @@ class App extends Component {
     // apiData is an array to hold our JSON data
     // isFetched indicates if the API call has finished
     // errorMsg is either null (none) or there is some error
-    //beginner property begins set to false
-    this.state = {apiData: [], apiDataBeginner: [], apiDataIntermediate: [], isFetched: false, errorMsg: null, isBeginner: false, isIntermediate: false};
+    //isBeginner/isIntermediate property begins set to false
+    this.state = {
+      apiData: [],
+      apiDataBeginner: [],
+      apiDataIntermediate: [],
+      isFetched: false,
+      errorMsg: null,
+      isBeginner: false,
+      isIntermediate: false
+    };
+
+    this.toggleBeginner = this.toggleBeginner.bind(this);
+    this.toggleIntermediate = this.toggleIntermediate.bind(this);
   }
-  //****************function called on button click, isBeginner = true*********************//
-  //****************this function can be copied for each button click********************//
+  //****************function called on button click, isBeginner/isIntermediate = true*********************//
+  //****************this function can be copied for each button********************//
   toggleBeginner = () => {
-    this.setState(state => ({isBeginner : !state.isBeginner}));
-  }
+    this.setState((state) => ({ isBeginner: !state.isBeginner }));
+  };
 
   toggleIntermediate = () => {
-    this.setState(state => ({isIntermediate : !state.isIntermediate}));
-  }
-  //****************function called on button click, isBeginner = true*********************//
+    this.setState((state) => ({ isIntermediate: !state.isIntermediate }));
+  };
+  //****************function called on button click, isBeginner/isIntermediate = true*********************//
 
-  //****************API calls starts here********************// 
+  //****************API calls starts here********************//
   // componentDidMount() is invoked immediately after a
   // component is mounted (inserted into the tree)
   async componentDidMount() {
@@ -37,7 +49,7 @@ class App extends Component {
 
       // update the state variables correctly.
       //access different parts of the json array by initializing this.setState and creating key:value pairs.
-      this.setState({ apiDataBeginner: jsonResult.Beginner});
+      this.setState({ apiDataBeginner: jsonResult.Beginner });
       this.setState({ apiDataIntermediate: jsonResult.Intermediate });
       this.setState({ isFetched: true });
     } catch (error) {
@@ -52,7 +64,7 @@ class App extends Component {
   // access specific properties from the JSON returned.
   // Notice that this time we have three possible returns for our
   // render. This is conditional rendering based on some conditions
-render() {
+  render() {
     if (this.state.errorMsg) {
       return (
         <div className="error">
@@ -67,78 +79,103 @@ render() {
       ); // end of return
     } else {
       // we have no errors and we have data
-//****************API calls end here********************//
+      //****************API calls end here********************//
 
+      //****************App return starts here********************//
+      return (
+        <div className="App">
+          {/*start of drop down button menu*/}
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenu2"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Please select your level
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <button
+              onClick={this.toggleBeginner}
+              className="dropdown-item"
+              type="button"
+            >
+              Beginner
+            </button>
+            <button
+              onClick={this.toggleIntermediate}
+              className="dropdown-item"
+              type="button"
+            >
+              Intermediate
+            </button>
+          </div>
+          {/*end of drop down button menu*/}
 
-//****************App return starts here********************//      
- return (
-      <div className="App">
-      {/*start of drop down button menu*/}
-      <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      Please select your level
-      </button>
-        <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-        <button onClick={this.toggleBeginner} className="dropdown-item" type="button">Beginner</button>
-        <button onClick={this.toggleIntermediate} className="dropdown-item" type="button">Intermediate</button>
+          {/*The below ternary operator works as follows. On Beginner button click it turns the isBeginner value to true. If the isBeginner is true 
+          the Beginner Componenent is called. Click the Beginner button again and it returns to false, in which case nothing is mapped.
+          Syntactically the ternary does this... is true ? do something : else something. First condition is executed after ? second condition after :*/}
+          {this.state.isBeginner ? (
+            <Beginner mapObjectBeginner={this.state.apiDataBeginner} />
+          ) : null}
+          {/*Beginner map ternary statement triggered by a drop down button click*/}
+
+          {/*card container starts with map function*/}
+          {/*The below ternary operator works as follows. On Intermediate button click it turns the isIntermediate value to true. If the isIntermedate is true 
+          the Intermediate Componenent is called. Click the Intermediate button again and it returns to false, in which case nothing is mapped.
+          Conceptually the ternary does this... is true ? do something : else something. First condition is executed after ? second condition after :*/}
+          {this.state.isIntermediate ? (
+            <Intermediate
+              mapObjectIntermediate={this.state.apiDataIntermediate}
+            />
+          ) : null}
+          {/*End of intermediate map ternary statement triggered by a drop down button click*/}
         </div>
-      {/*end of drop down button menu*/}
-
-        {/*card container starts with map function*/}
-        {/*Start of beginner map function triggered by a drop down button click. The button is a toggle button with two states, true and false.
-        The function is a ternary operator, if is beginner is true the card and its contents is rendered, otherwise null.
-        First click renders card content to the screen. Second click hides card*/}
-        {this.state.isBeginner ? 
-        <div className="card-group">
-                {this.state.apiDataBeginner.map((person, index) => (
-          <div className="card text-center">
-            <div class="card">
-                  <div className="card-body">
-                  <img className="card-img-top" alt="yogapic" src={person.imgURL} key={index}/>
-
-                    <h3 className="card-title">{person.body_part}</h3>
-                    <h5 className="car-title">{person.Position}</h5>
-                    <p className="card-text">{person.Description}</p>
-                    
-                    <audio controls autoplay>
-                    <source src={person.Audio}/>
-                    </audio>                                    
-                  </div>
-            </div>
-          </div>
-                ))}
-        </div> :null}
-        {/*End of level beginner map function triggered by a drop down button click*/}
-        {/*container ends*/}
-
-        {/*card container starts with map function*/}
-        {/*Start of intermediate map function triggered by a drop down button click. The button is a toggle button with two states, true and false. 
-        First click renders card content to the screen. Second click hides card*/}
-        {this.state.isIntermediate ? 
-        <div className="card-group">
-                {this.state.apiDataIntermediate.map((person, index) => (
-          <div className="card text-center">
-            <div class="card">
-                  <div className="card-body">
-                  <img className="card-img-top" alt="yogapic" src={person.imgURL} key={index}/>
-
-                    <h3 className="card-title">{person.body_part}</h3>
-                    <h5 className="car-title">{person.Position}</h5>
-                    <p className="card-text">{person.Description}</p>
-                    
-                    <audio controls autoplay>
-                    <source src={person.Audio}/>
-                    </audio>                                    
-                  </div>
-            </div>
-          </div>
-                ))}
-        </div> :null}
-        {/*End of intermediate map function triggered by a drop down button click*/}
-        {/*container ends*/}
-      </div>
       ); // end of return
     } // end of the else statement.
   } // end of render()
 } // end of App class
+
+//***************************Beginner Mapping Content Component****************************//
+class Beginner extends Component {
+  render() {
+    //this const declaration connects this Beginner class to the App class. It is the way to pass the//
+    //apiDataBeginner state to call the map function on it from within this component//
+    const mapBeginner = this.props.mapObjectBeginner;
+    return (
+      <div className="card-group">
+        {mapBeginner.map((person, index) => (
+          <div className="card text-center">
+            <div class="card">
+              <div className="card-body">
+                <img
+                  className="card-img-top"
+                  alt="yogapic"
+                  src={person.imgURL}
+                  key={index}
+                />
+
+                <h3 className="card-title">{person.body_part}</h3>
+                <h5 className="car-title">{person.Position}</h5>
+                <p className="card-text">{person.Description}</p>
+
+                <audio controls autoplay>
+                  <source src={person.Audio} />
+                </audio>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+//**************End of Beginner Component**********************//
+
+//**************Start of Intermediate Mapping Content Component***************//
+// The intermediate Component is now in its own file in the components folder and is imported into the App//
+//folder at the top of this App.js file//
+//**************End of Intermediate Mapping Content Component*************** */
 
 export default App;
